c9f9a814eeb6ce36a111b6835c8a828849271e9b
machiloop <hamidusodiq2@gmail.com>
2025-10-16 12:36:04 +0100

    feat(home): implement tab view layout

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,8 +1,65 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, useWindowDimensions, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { SceneMap, TabBar, TabView } from "react-native-tab-view";
+
+const firstRoute = () => <Text>hello1</Text>;
+
+const secondRoute = () => <Text>hello2</Text>;
+
+const thirdRoute = () => <Text>hello3</Text>;
+
+const renderScene = SceneMap({
+  petrol: firstRoute,
+  diesel: secondRoute,
+  kerosene: thirdRoute,
+});
+
+const routes = [
+  {
+    key: "petrol",
+    title: "Petrol",
+  },
+  {
+    key: "kerosene",
+    title: "Kerosene",
+  },
+  {
+    key: "diesel",
+    title: "Diesel",
+  },
+];
 
 const Home = () => {
-  return <Text>Home</Text>;
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1">
+        <TabView
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: layout.width }}
+          renderTabBar={(props) => (
+            <TabBar
+              {...props}
+              indicatorStyle={{ backgroundColor: "#007CEA" }}
+              style={{
+                backgroundColor: "white",
+                elevation: 0,
+                shadowOpacity: 0,
+                borderBottomWidth: 3,
+                borderBottomColor: "#CDE3F6",
+              }}
+              activeColor="#0082EC"
+              inactiveColor="black"
+            />
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;

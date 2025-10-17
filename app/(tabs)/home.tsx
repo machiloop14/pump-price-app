b949e43fe0430518093a2f6ac3f9b9c702c3266c
machiloop <hamidusodiq2@gmail.com>
2025-10-17 12:52:56 +0100

    feat(home): design header with filter button

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,8 +1,9 @@
 import DieselTab from "@/components/dieselTab";
 import KeroseneTab from "@/components/keroseneTab";
 import PetrolTab from "@/components/petrolTab";
+import { MaterialIcons } from "@expo/vector-icons";
 import React from "react";
-import { useWindowDimensions, View } from "react-native";
+import { Text, useWindowDimensions, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SceneMap, TabBar, TabView } from "react-native-tab-view";
 
@@ -32,6 +33,12 @@ const Home = () => {
   const [index, setIndex] = React.useState(0);
   return (
     <SafeAreaView className="flex-1 bg-white">
+      <View className="relative mt-4 mb-6">
+        <Text className="text-center font-bold text-2xl ">Fuel Finder</Text>
+        <View className="absolute right-4 -top-2 px-2 py-2 bg-[#DFEBF6] rounded-full">
+          <MaterialIcons name="tune" size={28} color="#007CEA" />
+        </View>
+      </View>
       <View className="flex-1">
         <TabView
           navigationState={{ index, routes }}

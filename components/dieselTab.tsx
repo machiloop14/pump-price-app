a5d1aceff65eb502dd500f08b2a13527982f54e0
machiloop <hamidusodiq2@gmail.com>
2025-10-20 08:32:56 +0100

    feat(ui): design diesel tab component

--- components/dieselTab.tsx
+++ components/dieselTab.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import dummydata from "../data/dummydata.json";
 
-const DieselTab = () => {
+import { Station } from "@/types/fuel";
+import PriceReport from "./priceReport";
+
+const dieselStations: Station[] = dummydata
+  .map((station) => ({
+    ...station,
+    prices: station.prices.filter(
+      (price) => price.fuelType.toLowerCase() === "diesel"
+    ),
+  }))
+  .filter((station) => station.prices.length > 0);
+
+const KeroseneTab = () => {
+  const [data, setData] = useState<Station[]>([]);
+
+  useEffect(() => {
+    setData(dieselStations);
+  }, [dieselStations]);
+
   return (
-    <View>
-      <Text>DieselTab</Text>
-    </View>
+    <ScrollView
+      className="flex flex-1 gap-4 mt-4"
+      showsVerticalScrollIndicator={false}
+    >
+      <PriceReport priceReport={data} />
+    </ScrollView>
   );
 };
 
-export default DieselTab;
+export default KeroseneTab;

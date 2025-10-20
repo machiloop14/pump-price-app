099ea40c370b9f51b8ce7259b08a83e3055739aa
machiloop <hamidusodiq2@gmail.com>
2025-10-20 08:31:06 +0100

    feat(ui): design kerosene tab component

--- components/keroseneTab.tsx
+++ components/keroseneTab.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import dummydata from "../data/dummydata.json";
+
+import { Station } from "@/types/fuel";
+import PriceReport from "./priceReport";
+
+const keroseneStations: Station[] = dummydata
+  .map((station) => ({
+    ...station,
+    prices: station.prices.filter(
+      (price) => price.fuelType.toLowerCase() === "kerosene"
+    ),
+  }))
+  .filter((station) => station.prices.length > 0);
 
 const KeroseneTab = () => {
+  const [data, setData] = useState<Station[]>([]);
+
+  useEffect(() => {
+    setData(keroseneStations);
+  }, [keroseneStations]);
+
   return (
-    <View>
-      <Text>KeroseneTab</Text>
-    </View>
+    <ScrollView
+      className="flex flex-1 gap-4 mt-4"
+      showsVerticalScrollIndicator={false}
+    >
+      <PriceReport priceReport={data} />
+    </ScrollView>
   );
 };
 
